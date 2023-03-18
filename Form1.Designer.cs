b582c3988aac0e73c18d9c6d9039a904b25e3837
Arendelle <648222852@qq.com>
2023-03-18 22:11:38 +0800

    firstcommit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace STF_Trainer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxTurn = new System.Windows.Forms.TextBox();
+            this.buttonTurn = new System.Windows.Forms.Button();
+            this.gameLoop = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonSaveFile = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 3000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "游戏进程状态：";
+            // 
+            // textBoxTurn
+            // 
+            this.textBoxTurn.Location = new System.Drawing.Point(98, 69);
+            this.textBoxTurn.Name = "textBoxTurn";
+            this.textBoxTurn.Size = new System.Drawing.Size(89, 25);
+            this.textBoxTurn.TabIndex = 1;
+            // 
+            // buttonTurn
+            // 
+            this.buttonTurn.Location = new System.Drawing.Point(215, 65);
+            this.buttonTurn.Name = "buttonTurn";
+            this.buttonTurn.Size = new System.Drawing.Size(91, 28);
+            this.buttonTurn.TabIndex = 2;
+            this.buttonTurn.Text = "修改";
+            this.buttonTurn.UseVisualStyleBackColor = true;
+            this.buttonTurn.Click += new System.EventHandler(this.buttonTurn_Click);
+            // 
+            // gameLoop
+            // 
+            this.gameLoop.Interval = 300;
+            this.gameLoop.Tick += new System.EventHandler(this.gameLoop_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "回合";
+            // 
+            // buttonSaveFile
+            // 
+            this.buttonSaveFile.Location = new System.Drawing.Point(203, 319);
+            this.buttonSaveFile.Name = "buttonSaveFile";
+            this.buttonSaveFile.Size = new System.Drawing.Size(130, 27);
+            this.buttonSaveFile.TabIndex = 4;
+            this.buttonSaveFile.Text = "打开存档目录...";
+            this.buttonSaveFile.UseVisualStyleBackColor = true;
+            this.buttonSaveFile.Click += new System.EventHandler(this.buttonSaveFile_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(345, 358);
+            this.Controls.Add(this.buttonSaveFile);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonTurn);
+            this.Controls.Add(this.textBoxTurn);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "ST:F Trainer by Arendelle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTurn;
+        private System.Windows.Forms.Button buttonTurn;
+        private System.Windows.Forms.Timer gameLoop;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonSaveFile;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.IO;
+
+namespace STF_Trainer
+{
+    enum ResourceTypes
+    {
+        Turn,
+        Credits,
+        Fuel
+    }
+    public partial class Form1 : Form
+    {
+        private bool GameProgressAvailable = false;
+        private string GameProcessName = "StarTradersFrontiers";
+        private IntPtr GameProcessHandle;
+        private IntPtr GameBaseAddress;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //从指定内存中读取字节集数据
+        [DllImportAttribute("kernel32.dll", EntryPoint = "ReadProcessMemory")]
+        public static extern bool ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, byte[] lpBuffer, int nSize, out IntPtr lpNumberOfBytesRead);
+        //从指定内存中写入字节集数据
+        [DllImportAttribute("kernel32.dll", EntryPoint = "WriteProcessMemory")]
+        public static extern bool WriteProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress, int[] lpBuffer, int nSize, IntPtr lpNumberOfBytesWritten);
+        [DllImportAttribute("kernel32.dll", EntryPoint = "OpenProcess")]
+        public static extern IntPtr OpenProcess(int dwDesiredAccess, bool bInheritHandle, int dwProcessId);
+
+        private static int ReadMemoryValue(IntPtr hProcess, IntPtr address)
+        {
+            try
+            {
+                byte[] buffer = new byte[4];
+                IntPtr bytesRead = IntPtr.Zero;
+                ReadProcessMemory(hProcess, address, buffer, buffer.Length, out bytesRead);
+                if (bytesRead == IntPtr.Zero) return -1;
+                return BitConverter.ToInt32(buffer, 0);
+            }
+            catch { return -1; }
+        }
+        private static int WriteMemoryValue(IntPtr hProcess, IntPtr address, int value)
+        {
+            try
+            {
+                WriteProcessMemory(hProcess, address, new int[] { value }, 4, IntPtr.Zero);
+                return 0;
+            }
+            catch { return -1; }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Process[] ps = Process.GetProcesses();
+            GameProgressAvailable = false;
+            foreach (Process p in ps)
+            {
+                if (p.ProcessName == GameProcessName)
+                {
+                    GameProgressAvailable = true;
+                    gameLoop.Enabled = true;
+                    label1.Text = "游戏进程状态：正常";
+                    GameProcessHandle = p.Handle;
+                    GameBaseAddress = p.MainModule.BaseAddress;
+                    Console.WriteLine("BaseAddr" + p.MainModule.BaseAddress);
+                    Console.WriteLine("EntryPointAddr" + p.MainModule.EntryPointAddress);
+
+                    Console.WriteLine(assembleResourceAddress(ResourceTypes.Turn));
+                }
+            }
+            if (!GameProgressAvailable) 
+            {
+                gameLoop.Enabled = false;
+                GameBaseAddress = IntPtr.Zero;
+                label1.Text = "游戏进程状态：未检测到";
+            }
+        }
+
+        int prevTurnValue = 0;
+        private void gameLoop_Tick(object sender, EventArgs e)
+        {
+            
+            if (GameBaseAddress != IntPtr.Zero) {
+                int valueTurn = resourceGet(ResourceTypes.Turn);
+                if (prevTurnValue != valueTurn)
+                {
+                    prevTurnValue = valueTurn;
+                    textBoxTurn.Text = (valueTurn - 35).ToString();
+                }
+            }
+        }
+
+        private int resourceGet(ResourceTypes res)
+        {
+            IntPtr resPtr = assembleResourceAddress(res);
+            int value = ReadMemoryValue(GameProcessHandle, resPtr);
+            return value;
+        }
+
+        private void resourceModify(ResourceTypes res, int value)
+        {
+            IntPtr resPtr = assembleResourceAddress(res);
+            WriteMemoryValue(GameProcessHandle, resPtr, value);
+
+        }
+
+        private IntPtr assembleResourceAddress(ResourceTypes res)
+        {
+            // 每种资源在游戏基址上的偏移量序列
+            int[,] offsetMap = { { 0x28, 0x14, 0x14, 0x08, 0x04, 0x14, 0xa4, 0x4c, 0xff },
+                                 { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0xff }};
+            
+            int tempPtr = ReadMemoryValue(GameProcessHandle, GameBaseAddress + 0x00852D1C);
+            int i = 0;
+            while (offsetMap[(int)res, i + 1] != 0xff) {
+                tempPtr = ReadMemoryValue(GameProcessHandle, (IntPtr)tempPtr + offsetMap[(int)res, i]);
+                i++;
+            }
+            tempPtr += offsetMap[(int)res, i];
+            return (IntPtr)tempPtr;
+        }
+
+        private void buttonTurn_Click(object sender, EventArgs e)
+        {
+            int newValue = int.Parse(textBoxTurn.Text);
+            resourceModify(ResourceTypes.Turn, newValue + 35);
+        }
+        private void buttonSaveFile_Click(object sender, EventArgs e)
+        {
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "\\StarTradersFrontiers";
+            if (Directory.Exists(path)) {
+                Process.Start(path);
+            }
+        }
+    }
+}
